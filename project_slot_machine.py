27751d94adefbf522035ab434ef83fcb22d22368
RajanSikarwar <rajansikarwar.ravindra@gmail.com>
2023-08-01 13:38:55 +0530

    deposit function

--- project_slot_machine.py
+++ project_slot_machine.py
@@ -1,3 +1,15 @@
 
 def deposit():
-    
+    while True:
+        amount = input("Enter amount to deposit: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount>0:
+                break
+            else:
+                print("Amount should be greater than 0.")
+        else:
+            print("Please enter a number.")
+    return amount
+
+deposit()
